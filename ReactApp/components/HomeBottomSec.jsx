96db459d638c2cdf2e22231252cc96e4185852df
anonymous6299 <ethicalanonymousgreyhat@gmail.com>
2024-11-09 15:49:43 +0530

    SearchTodoFeature

--- ReactApp/components/HomeBottomSec.jsx
+++ ReactApp/components/HomeBottomSec.jsx
@@ -2,19 +2,38 @@ import  { useEffect, useState } from 'react'
 import Todo from './Todo'
 const HomeBottomSec = (props) => {
     const [Arr, setArr] = useState([]);
+    const [Input, setInput] = useState("");
+    const [Todos, setTodos] = useState([]);
+    const [SearchResult, setSearchResult] = useState(null);
     useEffect(() => {
         const todos = props.todos;
+        setTodos(todos);
         const arr = todos.reverse();
         setArr(arr.slice(0,4));
     }, [props.todos])
-    
+    const onChange = (e) => {
+        if (e.target.value==="") {
+            setInput(e.target.value)
+            setSearchResult(null)
+        }
+        else{
+            setInput(e.target.value)
+        }
+    }
+    const onClick = (e) => {
+        e.preventDefault();
+        const data = Todos.find((item)=>{
+            return item.title===Input;
+        })
+        setSearchResult(data);
+    }
     return (
         <div className="pr-10">
             <div className='flex w-full justify-between'>
                 <p className='text-2xl font-medium mx-10'>Recently Added</p>
                 <div className="search flex">
-                    <input type="text" placeholder='Search' className='bg-transparent border border-[#212731] w-[18rem] h-9 rounded-l-md px-6 focus:outline-none focus:border-2 border-r-0' name='todo'/>
-                    <button className='border border-[#212731] rounded-r-md px-2'>
+                    <input type="text" placeholder='Search By Title' className='bg-transparent border border-[#212731] w-[18rem] h-9 rounded-l-md px-6 focus:outline-none focus:border-2 border-r-0' name='todo' value={Input} onChange={onChange} autoComplete='off'/>
+                    <button className='border border-[#212731] rounded-r-md px-2' onClick={onClick}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 opacity-70 hover:opacity-100">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
@@ -22,8 +41,9 @@ const HomeBottomSec = (props) => {
                 </div>
 
             </div>
-            <div className='w-full flex justify-between mx-4 my-16'>
+            <div className={`w-full flex ${SearchResult?"justify-center":"justify-between"} mx-4 my-16`}>
                 {
+                    SearchResult?<Todo props={{ item:SearchResult, bin: false }} />:
                     Arr.map((item,index)=>{
                         return <Todo key={index} props={{ item, bin: false }} />
                     })
--- ReactApp/components/TodoCompleted.jsx
+++ ReactApp/components/TodoCompleted.jsx
@@ -1,15 +1,19 @@
-import  { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const TodoCompleted = (props) => {
     const [CTodos, setCTodos] = useState([]);
+    const [Todos, setTodos] = useState([]);
     useEffect(() => {
-        const todos = props.todos;
-        let arr = todos.filter((item)=>{
-            return item.completed===true;
-        })
-        setCTodos(arr);
-    }, [])
-    
+        if (props.todos?.length !== 0) {
+            setTodos(props.todos)
+            const todos = props.todos;
+            let arr = todos.filter((item) => {
+                return item.completed === true;
+            })
+            setCTodos(arr);
+        }
+    }, [props.todos])
+
     return (
         <div className='todosCompleted bg-[#F9FAFB] border border-[#c1c4c9] text-[#212731] rounded-md h-fit w-[26rem] py-10 px-5 flex flex-col hover:shadow-lg'>
             <div className='flex justify-between'>
@@ -23,11 +27,13 @@ const TodoCompleted = (props) => {
                         26-50%: Orange (#F59E0B) to show moderate progress
                         51-75%: Yellow (#FBBF24) to reflect substantial progress
                         76-100%: Green (#10B981) to represent near or full completion */}
-                        <div className={`bg-[#EF4444] rounded-full w-[${(!(props.todos.length===0)&&CTodos.length!==0)?(CTodos.length/props.todos.length)*100:12}%] text-white text-center`}>{(!(props.todos.length===0))?(CTodos.length/props.todos.length)*100:"0"}%</div>
+                        <div className={`bg-[#EF4444] rounded-full text-white text-center`} style={{
+                            width: `${((Todos.length !== 0 && CTodos.length !== 0) ? (CTodos.length / props.todos.length) * 100 : 15)}%`
+                        }} >{(!(props.todos.length === 0)) ? (CTodos.length / props.todos.length) * 100 : "0"}%</div>
                     </div>
                 </div>
             </div>
-            <div className="leftTodo font-medium mt-2 text-center">{props.todos.length-CTodos.length} todos left</div>
+            <div className="leftTodo font-medium mt-2 text-center">{props.todos.length - CTodos.length} todos left</div>
         </div>
     )
 }
